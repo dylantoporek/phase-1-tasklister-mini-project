83984e3923655579ee2705796a47e40cd321d32c
DylanToporek <dylantoporek@gmail.com>
2021-09-28 11:25:32 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    let task = document.querySelector("#new-task-description")
+    e.preventDefault()
+    console.log(task.value);
+    buildToDo(task.value); 
+    form.reset()
+  })
+})
+
+function buildToDo(todo){
+  let p = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  p.textContent = `${todo}`
+  btn.textContent = ' x'
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
+document.querySelector("#new-task-description")
